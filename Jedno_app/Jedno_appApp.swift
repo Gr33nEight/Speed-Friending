d85db97afcdab668049e8407899970e294253b7c
Natanael Jop <natanael.jop@icloud.com>
2021-06-07 09:53:39 +0200

    Initial Commit

---
+++ Jedno_app/Jedno_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jedno_appApp.swift
+//  Jedno_app
+//
+//  Created by Natanael  on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Jedno_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
